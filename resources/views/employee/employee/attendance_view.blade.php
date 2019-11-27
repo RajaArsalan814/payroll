06918710396abd4d7ef72e7a71e157530599b84d
Raja Arsalan <rarsalan0@>
2019-11-27 12:43:47 -0800

    update

--- resources/views/employee/employee/attendance_view.blade.php
+++ resources/views/employee/employee/attendance_view.blade.php
@@ -49,28 +49,29 @@
                             @foreach ($item->attendance as $check)
                             <tr>
                                 <td  style="display:none">
-                                            {{$item->shifts->end_time}}
-                                            {{$item->check_out_time}}
-                                            {{-- Working Hours --}}
-                                            {{$check_start_time=Carbon\Carbon::parse($check->check_in_time)}}
-                                            {{$check_end_time=Carbon\Carbon::parse($check->check_out_time)}}
-                                            {{$working_hours=$check_end_time->diffInHours($check_start_time)}}
-                                            {{-- Over Time --}}
-                                            {{$shift_end_time=Carbon\Carbon::parse($item->shifts->end_time)}}
-                                            {{$check_out_time=Carbon\Carbon::parse($check->check_out_time)}}
-                                            {{$over_time=$shift_end_time->diffInHours($check_out_time)}}
-                                                {{-- Late in Time  --}}
-                                            {{$start_time=Carbon\Carbon::parse($item->shifts->start_time)}}
-                                            {{$check_in_time=Carbon\Carbon::parse($check->check_in_time)}}
-                                            {{$late_in_minutes=$check_in_time->diffInMinutes($start_time)}}
-                                            {{--  Early In Time --}}
-                                            {{$shift_start_time=Carbon\Carbon::parse($item->shifts->start_time)}}
-                                            {{$check_in_time=Carbon\Carbon::parse($check->check_in_time)}}
-                                            {{$early_in_minutes=$check_in_time->diffInMinutes($shift_start_time)}}
-                                            </td>
+                                {{$item->shifts->end_time}}
+                                {{$item->check_out_time}}
+                                {{-- Working Hours --}}
+                                {{$my_check_out_time=Carbon\Carbon::parse($check->check_out_time)->format('H:i')}}
+                                {{$check_start_time=Carbon\Carbon::parse($check->check_in_time)->format('H:i')}}
+                                {{$check_end_time=Carbon\Carbon::parse($check->check_out_time)}}
+                                {{$working_hours=$check_end_time->diffInHours($check_start_time)}}
+                                {{-- Over Time --}}
+                                {{$shift_end_time=Carbon\Carbon::parse($item->shifts->end_time)}}
+                                {{$check_out_time=Carbon\Carbon::parse($check->check_out_time)}}
+                                {{$over_time=$shift_end_time->diffInHours($check_out_time)}}
+                                    {{-- Late in Time  --}}
+                                {{$start_time=Carbon\Carbon::parse($item->shifts->start_time)}}
+                                {{$check_in_time=Carbon\Carbon::parse($check->check_in_time)}}
+                                {{$late_in_minutes=$check_in_time->diffInMinutes($start_time)}}
+                                {{--  Early In Time --}}
+                                {{$shift_start_time=Carbon\Carbon::parse($item->shifts->start_time)}}
+                                {{$check_in_time=Carbon\Carbon::parse($check->check_in_time)}}
+                                {{$early_in_minutes=$check_in_time->diffInMinutes($shift_start_time)}}
+                                </td>
                             <td>{{$check->attendance_date}}</td>
-                            <td>{{$check->check_in_time}}</td>
-                            <td>{{$check->check_out_time}}</td>
+                            <td>{{$check_start_time}}</td>
+                            <td>{{$my_check_out_time}}</td>
                             <td>{{$over_time}}</td>
                             <td>{{$working_hours}}</td>
                             @if($check_in_time > $start_time)
@@ -83,8 +84,10 @@
                             @else
                             <td>0</td>
                             @endif
-                            @if($check_in_time )
+                            @if($check_in_time  AND $check_out_time >= $item->shifts->end_time)
                             <td>Present</td>
+                            @elseif($check_in_time)
+                            <td>Half Day</td>
                             @else
                             <td>Absent</td>
                             @endif
@@ -94,9 +97,43 @@
                     </tr>
                 </table>
 
-                    <div class="container">
-                        <h2>asdasd</h2>
-                    </div>
+                    {{--  <div class="container">
+                        <h5>Summary</h5>
+                        <div class="row">
+                            <div class="col-md-4">
+
+                                <p>No Of Presents :
+                                        <span>{{$check->count()}}</span>
+                                    </p>
+                            </div>
+                                <div class="col-md-4">
+                                        <p>No Of Absents : 2</p>
+                                    </div>
+                                    <div class="col-md-4">
+                                            <p>No Of Leaves : 2</p>
+                                        </div>
+                                        <div class="col-md-4">
+                                                <p>No Of Late Arrivals : 2</p>
+                                            </div>
+                                            <div class="col-md-4">
+                                                    <p>No Of Half Days : 2</p>
+                                                </div>
+
+                                                <div class="col-md-4">
+                                                        <p>No Of Early Departure : 2</p>
+                                                    </div>
+                                                    <div class="col-md-4">
+                                                            <p>No Of Hours (O.T) : 2</p>
+                                                        </div>
+                                                        <div class="col-md-4">
+                                                                <p>No Of Holidays : 2</p>
+                                                            </div>
+
+                                                        <div class="col-md-4">
+                                                                <p>No Of Holidays Half Days : 2</p>
+                                                            </div>
+                        </div>
+                    </div>  --}}
 
             </div>
         </div>
